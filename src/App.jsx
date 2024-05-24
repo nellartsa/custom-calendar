cb120c35428a6addb20db34ea95d3c111972ff91
Allen Astral De Jesus <dj.allenastral@gmail.com>
2024-05-24 14:05:39 +0800

    deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-import { useState } from "react";
 import DatePicker, { Calendar } from "./DatePicker";
 import "./App.css";
 
 function App() {
-  const [dateSelected, setDateSelected] = useState(new Date());
-
   return (
     <>
       <Calendar />
